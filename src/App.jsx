efb53f0a19252d4370b377937c278e93545655cd
Purna Shrestha <crush9918@gmail.com>
2023-07-25 09:34:57 +0545

    Remove Boiler plates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React from "react";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
