e5393de9fe14dd4ff6a9a76a45dc8712673a8a34
Purna Shrestha <crush9918@gmail.com>
2023-09-09 13:29:11 +0545

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./index.css";
+import Navbar from "./assets/components/Navbar";
 
 function App() {
   return (
     <>
-
+      <Navbar />
     </>
   );
 }
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      Vite + React & TailwindCSS Starter Template
+    </div>
+  );
+}
+
+export default Navbar;
