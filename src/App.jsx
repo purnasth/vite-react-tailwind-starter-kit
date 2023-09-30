2028a4d6a02440c1eb0685326a0d6826af9c2654
Purna Shrestha <crush9918@gmail.com>
2023-09-30 18:14:46 +0545

    Fixthe src of the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import Navbar from "./assets/components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div>
+    <>
       Vite + React & TailwindCSS Starter Template
-    </div>
+    </>
   );
 }
 
