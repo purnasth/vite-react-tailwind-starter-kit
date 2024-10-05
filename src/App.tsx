7c0b022ef53e46a2d046e787a2ea4338d73b787a
Purna Shrestha <crush9918@gmail.com>
2024-10-05 12:47:28 +0545

    feat: Add typescript and pnpm feature with prettier

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <main className="flex h-screen w-full items-center justify-center">
+        <h1 className="text-center font-serif text-5xl capitalize">
+          Welcome to Vite + React and tailwindCSS starter Kit
+        </h1>
+      </main>
+    </>
+  );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,31 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-    },
     extend: {
-      backgroundImage: {
-        "radial-gradient-orange":
-          "radial-gradient(circle, rgba(255,217,184,1) 0%, rgba(255,159,82,1) 100%)",
-      },
-      fontFamily: {
-        playfair: ['"Playfair Display"', "serif"],
-        inria: ['"Inria Serif"', "serif"],
-        rambla: ['"Rambla"', "sans-serif"],
-        kalnia: ['"Kalnia"', "sans-serif"],
-      },
       colors: {
-        "custom-blue": "#070a0f",
-        "custom-orange": "#DB6332",
-        "custom-white": "#F2F3D9",
-        dark: {
-          DEFAULT: "#1a202c",
-        },
+        dark: "#111111",
+      },
+      fontSize: {
+        inherit: "inherit",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1600px",
+        "3xl": "1920px",
       },
       container: {
+        center: true,
+        padding: "1rem",
         screens: {
           sm: "100%",
           md: "100%",
           lg: "1024px",
           xl: "1280px",
+          "2xl": "1600px",
         },
       },
     },
